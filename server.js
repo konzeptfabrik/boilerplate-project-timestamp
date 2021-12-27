3861a6042ccc5f35780d99c19edbdeea5b2e9851
konzeptfabrik <carsten.lebek@googlemail.com>
2021-12-27 11:58:48 +0100

    done

--- server.js
+++ server.js
@@ -2,41 +2,38 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/api/:date", function (req, res) {
-  const date = new Date(req.params.date);
+	const date = isNaN(req.params.date)
+		? new Date(req.params.date)
+		: new Date(req.params.date * 1000);
 
-  if (!date) {
-    res.json({ error: "Invalid Date" })
-  }
+	const unix = parseInt((date.getTime() / 1000).toFixed(0));
 
-  const unix = parseInt((date.getTime() / 1000).toFixed(0))
+	const utc = date.toUTCString();
 
-  const utc = date.toUTCString()
-
-  res.json({
-    unix: unix,
-    utc: utc
-  })
-})
-
+	res.json({
+		unix: unix,
+		utc: utc,
+	});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+	console.log("Your app is listening on port " + listener.address().port);
 });
